d0d466b9beea789adf7c03e5e94f37d4bf1883b8
Kareem727 <kareem.ga.13@gmail.com>
2022-03-20 15:29:48 +0200

    _o3

---
+++ src/components/Loading.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import './Loading.scss'
+function Loading() {
+  return (
+
+<div className="preloader">
+  <div className="preloader__ring">
+    <div className="preloader__sector">L</div>
+    <div className="preloader__sector">o</div>
+    <div className="preloader__sector">a</div>
+    <div className="preloader__sector">d</div>
+    <div className="preloader__sector">i</div>
+    <div className="preloader__sector">n</div>
+    <div className="preloader__sector">g</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+  </div>
+  <div className="preloader__ring">
+    <div className="preloader__sector">D</div>
+    <div className="preloader__sector">R</div>
+    <div className="preloader__sector">I</div>
+    <div className="preloader__sector">V</div>
+    <div className="preloader__sector">E</div>
+    <div className="preloader__sector"><p></p></div>
+    <div style={{color: "black", fontWeight: '900', fontSize: '43px'}} className="preloader__sector">7</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector">.</div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+    <div className="preloader__sector"></div>
+  </div>
+</div>
+
+    )
+}
+
+export default Loading
--- src/components/hellow.js
+++ src/components/hellow.js
@@ -1,65 +1,91 @@
-import React from 'react'
-import { useState ,useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Loading from "./Loading";
 
-const  Hellow = (props) =>{
+const Hellow = (props) => {
+  const token = "563492ad6f91700001000001c4f8b66bbb93484b909869982c94635e";
 
-const token = '563492ad6f91700001000001c4f8b66bbb93484b909869982c94635e';
-    
-    // const {id} = useParams();
-    let content = null
-    const [product, setProduct] =useState(null);
-    const[imagee, setImagee] =useState(null);
- const url =`https://swapi.dev/api/people/1`
- const url2 = "https://api.pexels.com/v1/curated?per_page=1"
+  // const {id} = useParams();
+  let content = null;
 
- useEffect(() =>{
-   axios.get(url2, {
-    headers: {
-        'Authorization': `${token}`
-    }
-  }).then(response =>{
-       setImagee(response.data);
-   })
- },[url2]);
+  const url = `https://swapi.dev/api/people/1`;
+  const url2 = "https://api.pexels.com/v1/curated?per_page=1";
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+  });
 
-useEffect(()=>{
-axios.get(url).then(response => {
- setProduct(response.data);
-})
-},[url]);
+  const [imagee, setImagee] = useState({
+    loading: false,
+    data: null,
+  });
+  useEffect(() => {
+    setImagee({
+      loading: true,
+      data: null,
+    });
+    axios
+      .get(url2, {
+        headers: {
+          Authorization: `${token}`,
+        },
+      })
+      .then((response) => {
+        setImagee({
+          loading: false,
+          data: response.data,
+        });
+      });
+  }, [url2]);
 
+  useEffect(() => {
+    setProduct({
+      loading: true,
+      data: null,
+    });
+    axios.get(url).then((response) => {
+      setProduct({
+        loading: false,
+        data: response.data,
+      });
+    });
+  }, [url]);
 
+  if(imagee.loading){
+content =
+<Loading/>
+}
 
-if(product && imagee){
-    content= 
-    <div>
+  if (product.data && imagee.data) {
+    content = (
+      <div>
         <div>
-            <img style={{width: 200, height: 200,}} src={imagee.photos[0].src.original}></img>
+          <img
+            style={{ width: 200, height: 200 }}
+            src={imagee.data.photos[0].src.original}
+          ></img>
         </div>
         <div>
-             <h1>{product.name}</h1>
-         </div>
+          <h1>{product.data.name}</h1>
+        </div>
 
-         <div>
-             <h1>{product.height}</h1>
-         </div>
+        <div>
+          <h1>{product.data.height}</h1>
+        </div>
 
-         <div>
-             <h1>{product.mass}</h1>
-         </div>
+        <div>
+          <h1>{product.data.mass}</h1>
+        </div>
 
-         <div>
-             <h1>{product.eye_color}</h1>
-         </div>
-    </div>
-}
-  return (
-      <div>
-          {content}
+        <div>
+          <h1>{product.data.eye_color}</h1>
+        </div>
       </div>
-   )
-}
+    );
+  }
+  return <div>{content}</div>;
+};
 
 export default Hellow;
