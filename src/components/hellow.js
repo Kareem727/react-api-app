8a0c48f427e9be836a9ce78e6feda246e13302dd
Kareem727 <kareem.ga.13@gmail.com>
2022-03-20 09:29:47 +0200

    _o_

--- src/components/hellow.js
+++ src/components/hellow.js
@@ -1,26 +1,41 @@
 import React from 'react'
 import { useState ,useEffect } from 'react';
 import axios from 'axios';
+// import { useParams } from 'react-router-dom';
 const  Hellow = (props) =>{
-    const url ="https://swapi.dev/api/people/1"
- const [product, setProduct] =useState();
- 
- useEffect(()=>{
-  axios.get(url).then( response =>{
-      setProduct(response.data)
-  })
- },[url])
+    const [product, setProduct] =useState();
+ const url =`https://swapi.dev/api/people/1`
 
- if(product){
-     return(
-         <div>
+useEffect(()=>{
+axios.get(url).then(response => {
+ setProduct(response.data);
+})
+},[url]);
+let content = null
+
+if(product){
+    content= 
+    <div>
+        <div>
              <h1>{product.name}</h1>
          </div>
-     )
- }
+
+         <div>
+             <h1>{product.height}</h1>
+         </div>
+
+         <div>
+             <h1>{product.mass}</h1>
+         </div>
+
+         <div>
+             <h1>{product.eye_color}</h1>
+         </div>
+    </div>
+}
   return (
       <div>
-
+          {content}
       </div>
    )
 }
